d4571c4757770b708519a68f18ded5df3e39be0d
Martin Moene <martin.moene@gmail.com>
2014-12-24 18:27:36 +0100

    Update lest_cpp03.hpp (1.19.1)

--- lest_cpp03.hpp
+++ lest_cpp03.hpp
@@ -33,7 +33,7 @@
 # pragma GCC   diagnostic ignored "-Wunused-value"
 #endif
 
-#define  lest_VERSION "1.19.0"
+#define  lest_VERSION "1.19.1"
 
 #ifndef  lest_FEATURE_COLOURISE
 # define lest_FEATURE_COLOURISE 0
