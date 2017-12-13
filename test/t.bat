4cff0a0abf8961fffbdfc5e815e160bdd9de4ca8
Martin Moene <martin.moene@gmail.com>
2017-12-13 10:13:16 +0100

    Re-add optional C++ standard argument

--- test/t.bat
+++ test/t.bat
@@ -1,6 +1,6 @@
 @setlocal
 @set std=%1
 @if not "%std%"=="" set std=-std:%std%
-cl -W3 -EHsc %std% -I../include/nonstd optional-lite.t.cpp optional.t.cpp && optional-lite.t.exe
+cl -W3 -EHsc %std% -Doptional_EXPERIMENTAL_FUNCTIONAL_EXTENSTIONS -I../include/nonstd optional-lite.t.cpp optional.t.cpp && optional-lite.t.exe
 @endlocal
 
