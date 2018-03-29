9c9ab7907ecac4e96caca21ebb4d270023e1a124
Martin Moene <martin.moene@gmail.com>
2018-03-29 15:48:04 +0200

    Add more warnings

--- test/tg.bat
+++ test/tg.bat
@@ -1,6 +1,9 @@
-@setlocal
-@set std=%1
-@if "%std%"=="" set std=c++11
-g++ -std=%std% -O2 -Wall -Wextra -Wno-unused-parameter -o optional-main.t.exe -I../include/nonstd optional-main.t.cpp optional.t.cpp && optional-main.t.exe
-@endlocal
+@echo off & setlocal
+set std=%1
+if "%std%"=="" set std=c++14
 
+set flags=-Wpedantic -Wno-padded -Wno-missing-noreturn
+set   gpp=g++
+
+%gpp% -std=%std% -O2 -Wall -Wextra %flags% -o optional-main.t.exe -I../include/nonstd optional-main.t.cpp optional.t.cpp && optional-main.t.exe
+endlocal
