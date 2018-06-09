e04439e2f1bd32cefd86a015c824235f613ab432
Martin Moene <martin.moene@gmail.com>
2018-06-09 12:31:58 +0200

    Declare stream operator before inclusion of lest test framework

--- test/optional-main.t.h
+++ test/optional-main.t.h
@@ -2,7 +2,7 @@
 //
 // https://github.com/martinmoene/optional-lite
 //
-// Distributed under the Boost Software License, Version 1.0. 
+// Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
@@ -11,6 +11,10 @@
 #define TEST_OPTIONAL_LITE_H_INCLUDED
 
 #include "optional.hpp"
+
+#include <iosfwd>
+namespace lest { template<typename T> std::ostream & operator<<( std::ostream & os, nonstd::optional<T> const & v ); }
+
 #include "lest_cpp03.hpp"
 
 using namespace nonstd;
@@ -19,23 +23,14 @@ using namespace nonstd;
 
 extern lest::tests & specification();
 
-namespace nonstd { namespace optional_lite {
-
-// use oparator<< instead of to_string() overload;
-// see  http://stackoverflow.com/a/10651752/437272
+namespace lest {
 
 template< typename T >
-inline std::ostream & operator<<( std::ostream & os, optional<T> const & v )
+inline std::ostream & operator<<( std::ostream & os, nonstd::optional<T> const & v )
 {
     using lest::to_string;
     return os << "[optional:" << (v ? to_string(*v) : "[empty]") << "]";
 }
-
-}}
-
-namespace lest {
-
-using ::nonstd::optional_lite::operator<<;
 
 } // namespace lest
 
