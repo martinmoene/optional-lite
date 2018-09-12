67bd848201d381962dbf16bad8bfb27f09da3f42
Martin Moene <martin.moene@gmail.com>
2018-09-12 22:00:39 +0200

    Add -Wconversion warning

--- test/tg.bat
+++ test/tg.bat
@@ -2,7 +2,7 @@
 set std=%1
 if "%std%"=="" set std=c++14
 
-set flags=-Wpedantic -Wno-padded -Wno-missing-noreturn
+set flags=-Wpedantic -Wconversion -Wno-padded -Wno-missing-noreturn
 set   gpp=g++
 
 %gpp% -std=%std% -O2 -Wall -Wextra %flags% -o optional-main.t.exe -I../include/nonstd optional-main.t.cpp optional.t.cpp && optional-main.t.exe
