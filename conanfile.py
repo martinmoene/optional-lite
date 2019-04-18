84783d858f0e1a61890046c6623393629a8657a4
Martin Moene <martin.moene@gmail.com>
2019-04-18 08:41:18 +0200

    Improve conan/cmake interoperation (nonstd-lite issue 32)
    - https://github.com/martinmoene/nonstd-lite-project/issues/32

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 class OptionalLiteConan(ConanFile):
     version = "3.1.1"
@@ -6,8 +6,8 @@ class OptionalLiteConan(ConanFile):
     description = "A single-file header-only version of a C++17-like optional, a nullable object for C++98, C++11 and later"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
     url = "https://github.com/martinmoene/optional-lite.git"
-    exports_sources = "include/nonstd/*", "LICENSE.txt"
-    build_policy = "missing"    
+    exports_sources = "include/nonstd/*", "CMakeLists.txt", "cmake/*", "LICENSE.txt"
+    build_policy = "missing"
     author = "Martin Moene"
 
     def build(self):
@@ -15,8 +15,12 @@ class OptionalLiteConan(ConanFile):
         pass
 
     def package(self):
-        """Provide pkg/include/nonstd/*.hpp"""
-        self.copy("*.hpp")
+        """Run CMake install"""
+        cmake = CMake(self)
+        cmake.definitions["OPTIONAL_LITE_OPT_BUILD_TESTS"] = "OFF"
+        cmake.definitions["OPTIONAL_LITE_OPT_BUILD_EXAMPLES"] = "OFF"
+        cmake.configure()
+        cmake.install()
 
     def package_info(self):
         self.info.header_only()
